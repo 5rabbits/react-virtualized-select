97d41d76bfc50e45bff6b98933fa1f4ed8e3fd3f
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2016-08-08 16:41:27 -0400

    Tracking dist folder

---
+++ dist/es/VirtualizedSelect/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _VirtualizedSelect = require('./VirtualizedSelect');
+
+var _VirtualizedSelect2 = babelHelpers.interopRequireDefault(_VirtualizedSelect);
+
+exports.default = _VirtualizedSelect2.default;
